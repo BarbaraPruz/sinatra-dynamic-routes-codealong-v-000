2cc048bb32fedf7e110fe82ac8cfef4f433c5f52
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-16 22:56:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,8 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}"
   end
-  get "multiply/:num1/:num2" do
+
+  get "/multiply/:num1/:num2" do
     @n1 = params[:num1].to_i
     @n2 = params[:num2].to_i
     "Product is #{@n1 * @n2}"
